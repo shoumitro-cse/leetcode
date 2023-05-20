306db0dc85a317c5d13f2f7cc0ca42b282e85994
Shoumitro Ray <shoumitro26@gmail.com>
2023-05-20 15:14:37 +0600

    lazy loading class ex added

--- lazy_loading/keep_lazy.py
+++ lazy_loading/keep_lazy.py
@@ -1,5 +1,5 @@
-""" keep_lazy() decorator example
-
+""" 
+keep_lazy() decorator example
 Certainly! Here's an example of a keep_lazy() decorator in Python:
 """ 
 
@@ -19,6 +19,7 @@ def keep_lazy(func):
         return lazy_func
 
     return lazy_func
+    
 
 """ 
 This decorator allows you to delay the execution of a function until its result is explicitly requested. Here's how you can use it:
@@ -62,6 +63,52 @@ def truncate_words(s, num, end_text='...'):
 
 
 truncate_words = keep_lazy(truncate_words, six.text_type)
+
+
+
+
+"""
+Lazy class property decorator
+Certainly! Here's an example of a lazy_class_property decorator in Python, which allows for lazy evaluation of class properties:
+"""
+
+import functools
+
+def lazy_class_property(func):
+    """Decorator to implement lazy evaluation of class properties."""
+
+    @property
+    @functools.wraps(func)
+    def lazy_loader(cls):
+        if not hasattr(cls, "_lazy_cache"):
+            cls._lazy_cache = {}
+        if func.__name__ not in cls._lazy_cache:
+            cls._lazy_cache[func.__name__] = func(cls)
+        return cls._lazy_cache[func.__name__]
+
+    return lazy_loader
+
+"""
+To use this decorator, you can apply it to a class method that serves as a getter for a class property. The property will be computed lazily, meaning it will be evaluated only when accessed for the first time. Here's an example:
+"""
+
+class MyClass:
+    @lazy_class_property
+    def my_property(cls):
+        print("Computing my_property...")
+        return 42
+
+# Accessing the class property triggers the lazy evaluation
+print(MyClass.my_property)  # Computing my_property... 42
+
+# Accessing the class property again does not recompute it
+print(MyClass.my_property)  # 42
+
+"""
+In this example, the my_property method is decorated with @lazy_class_property. The first time MyClass.my_property is accessed, the decorator will execute the method and store the result in the _lazy_cache dictionary specific to the class. Subsequent accesses to MyClass.my_property will return the cached value without recomputing it.
+
+The lazy_class_property decorator ensures that the property is evaluated lazily and shared among all instances of the class. If you need a per-instance lazy property, you can modify the decorator to store the cached values in an instance-specific dictionary instead of a class-level dictionary.
+"""
 
 
 
